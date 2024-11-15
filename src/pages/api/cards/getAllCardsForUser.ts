b196d27752136f07dc986615c43dd201293df89f
SleepyWoodpecker <shawnweichew@gmail.com>
2024-11-15 09:33:17 -0800

    refactor: used native url params option and added more error handling

--- src/pages/api/cards/getAllCardsForUser.ts
+++ src/pages/api/cards/getAllCardsForUser.ts
@@ -3,7 +3,7 @@ import MongoDB from "../services/db/mongoDB";
 import DB from "../services/db/DB";
 import { Card } from "@/types/card";
 import { ErrorResponse } from "@/types/apiError";
-import { parseUrlParams } from "../services/helper";
+import { BSON } from "mongodb";
 
 // for now I am creating a new instance of the DB everytime, but its reusing the same connection
 // not sure if this is the best way to be doing this honestly
@@ -17,17 +17,35 @@ export default async function handler(
   const fullUrl = new URL(
     `http://${process.env.HOST ?? "localhost"}${req.url}`,
   );
-  const userIdString = fullUrl.searchParams.get("userIdString");
+  const userIdString = req.query.userIdString as string | undefined;
 
   if (!userIdString) {
     return res.status(400).json({
       error: true,
       errorMessage: "Missing required arguments",
-      params: parseUrlParams(fullUrl.searchParams),
+      params: req.query,
       route: fullUrl,
     });
   }
 
-  const allCards = await db.getAllCardsForUser(userIdString);
-  return res.status(200).json(allCards);
+  try {
+    const allCards = await db.getAllCardsForUser(userIdString);
+    return res.status(200).json(allCards);
+  } catch (err) {
+    if (err instanceof BSON.BSONError)
+      return res.status(404).json({
+        error: true,
+        errorMessage: `userId: ${userIdString} not found`,
+        params: req.query,
+        route: fullUrl,
+      });
+
+    console.error(err);
+    return res.status(500).json({
+      error: true,
+      errorMessage: `Internal server error`,
+      params: req.query,
+      route: fullUrl,
+    });
+  }
 }
