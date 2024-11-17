d7888d9e360d520ac89accc68537e60e1ae5b1f9
SleepyWoodpecker <shawnweichew@gmail.com>
2024-11-16 17:30:45 -0800

    updated types, again *sad face*

--- src/types/pageFeatures.ts
+++ src/types/pageFeatures.ts
@@ -2,48 +2,73 @@ import { z } from "zod";
 
 // const zodPageFeature = z.enum([])
 
-export const zodFeatureType = z.enum(["spotifyPlaylistUrl", "image", "text"]);
+export const zodFeatureType = z.enum(["header", "body", "asset"]);
 export type FeatureType = z.infer<typeof zodFeatureType>;
-
-export const zodTextType = z.enum(["header", "body"]);
-export type TextType = z.infer<typeof zodTextType>;
-
-export const zodImageFeatureType = z.enum(["single", "fan", "carousell"]);
-export type ImageFeatureType = z.infer<typeof zodImageFeatureType>;
 
 export const zodBaseFeature = z.object({
   featureType: zodFeatureType,
 });
 export type BaseFeature = z.infer<typeof zodBaseFeature>;
 
-export const zodTextFeature = zodBaseFeature.extend({
-  featureType: z.literal(zodFeatureType.enum.text),
-  textContent: z.string(),
-  textColor: z.string(),
-  textType: zodTextType,
+export const zodHeaderFeature = zodBaseFeature.extend({
+  featureType: z.literal(zodFeatureType.enum.header),
+  textContent: z.string().default(""),
+  textColor: z.string().default("black"),
 });
-export type TextFeature = z.infer<typeof zodTextFeature>;
+export type HeaderFeature = z.infer<typeof zodHeaderFeature>;
 
-export const zodImageFeature = zodBaseFeature.extend({
-  featureType: z.literal(zodFeatureType.enum.image),
-  imageUrls: z.array(z.string()),
-  imageFeatureType: zodImageFeatureType,
+export const zodBodyFeature = zodBaseFeature.extend({
+  textContent: z.string().default(""),
+  textColor: z.string().default("black"),
 });
+export type BodyFeature = z.infer<typeof zodBodyFeature>;
 
-export type ImageFeature = z.infer<typeof zodImageFeature>;
+export const zodAssetType = z.enum(["image", "spotifyPplaylistUrl"]);
+export type AssetType = z.infer<typeof zodAssetType>;
+
+export const zodAssetFeature = zodBaseFeature.extend({
+  featureType: z.literal(zodFeatureType.enum.asset),
+  assetType: zodAssetType,
+});
+export type AssetFeature = z.infer<typeof zodAssetFeature>;
+
+export const zodImageFeatureType = z.enum(["single", "fan", "carousell"]);
+export type ImageFeatureType = z.infer<typeof zodImageFeatureType>;
+
+export const zodSingleImageFeature = zodAssetFeature.extend({
+  assetType: z.literal(zodAssetType.enum.image),
+  imageFeatureType: z.literal(zodImageFeatureType.enum.single),
+  imageUrl: z.string().url(),
+});
+export type SingleImageFeature = z.infer<typeof zodSingleImageFeature>;
+
+// TODO: type still under construction
+export const zodFanImageFeature = zodAssetFeature.extend({
+  assetType: z.literal(zodAssetType.enum.image),
+  imageFeatureType: z.literal(zodImageFeatureType.enum.fan),
+  imageUrl: z.array(z.string().url()),
+});
+export type FanImageFeature = z.infer<typeof zodFanImageFeature>;
+
+// TODO: type still under construction
+export const zodCarousellImageFeature = zodAssetFeature.extend({
+  assetType: z.literal(zodAssetType.enum.image),
+  imageFeatureType: z.literal(zodImageFeatureType.enum.carousell),
+  imageUrl: z.array(z.string().url()),
+});
+export type CarouSellImageFeature = z.infer<typeof zodCarousellImageFeature>;
 
 // Spotify Playlist URL Feature Schema
-export const zodSpotifyPlaylistUrlFeature = zodBaseFeature.extend({
-  featureType: z.literal(zodFeatureType.enum.spotifyPlaylistUrl),
-  playlistUrl: z.string(),
+export const zodSpotifyPlaylistUrlFeature = zodAssetFeature.extend({
+  assetType: z.literal(zodAssetType.enum.spotifyPplaylistUrl),
+  playlistUrl: z.string().url(),
 });
-
 export type SpotifyPlaylistUrl = z.infer<typeof zodSpotifyPlaylistUrlFeature>;
 
 export const zodPageFeature = z.union([
-  zodImageFeature,
-  zodTextFeature,
-  zodSpotifyPlaylistUrlFeature,
+  zodBodyFeature,
+  zodHeaderFeature,
+  zodAssetFeature,
 ]);
 
 export type PageFeature = z.infer<typeof zodPageFeature>;
